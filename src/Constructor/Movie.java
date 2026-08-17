668ab87024115289b3c1ee7bdd80b20c80bb690b
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-08-17 23:48:02 +0530

    Changes

---
+++ src/Constructor/Movie.java
@@ -0,0 +1,24 @@
+package Constructor;
+
+public class Movie {
+    int id;
+    String movieName;
+    String director;
+
+    Movie(int id , String movieName, String director){
+        this.id= id;
+        this.movieName=movieName;
+        this.director = director;
+
+    }
+
+    public static void main(String[] args){
+        Movie m = new Movie(101,"KGF", "Rajmoli");
+        System.out.println(m.id);
+        System.out.println(m.director);
+        System.out.println(m.movieName);
+
+        Movie m1 = new Movie(102,"RRR", "Tiwari");
+        System
+    }
+}
