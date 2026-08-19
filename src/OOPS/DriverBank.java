e5b4b3684ac3826b8a6e7c9b5e0e53b64564de6c
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-08-20 00:27:15 +0530

    Learning OOPS concept Verification and validation if Data

---
+++ src/OOPS/DriverBank.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+public class DriverBank {
+    public static void main(String[] args){
+        Bank b1=new Bank();
+        b1.setBankId(101);
+        b1.setEmail("sbi@gmail.com");
+        b1.setBalance(70000.0);
+
+        Bank b2 = new Bank();
+        b2.setBankId(201);
+        b2.setEmail("hdfc@gmail.com");
+        b2.setBalance(40000.0);
+
+        System.out.println(b1.getBankId());  //101
+        System.out.println(b1.getEmail(101));   //sbi@gmail.com
+        System.out.println(b1.getBalance());  //70000.0
+
+    }
+}
