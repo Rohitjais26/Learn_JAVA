ea9dd5941c541847b61f8227c2b46c696504557b
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-08-24 22:55:41 +0530

    Learning OOPS concept like has-a-relationship Compound Outputs.

--- src/OOPS/LaptopDriver.java
+++ src/OOPS/LaptopDriver.java
@@ -5,11 +5,11 @@ public class LaptopDriver {
         Laptop1 l = new Laptop1();
         l.setId(201).setlBrand("HP");
         l.getB().setbId(101).setBrand("M2");
-        System.out.println(l.getlId());
-        System.out.println(l.getlBrand());
-        System.out.println(l.getB());
-        System.out.println(l.getB().getbId());
-        System.out.println(l.getB().getBrand());
+        System.out.println(l.getlId()); //201
+        System.out.println(l.getlBrand()); //HP
+        System.out.println(l.getB());  //OOPS.Battery@5caf905d
+        System.out.println(l.getB().getbId()); //101
+        System.out.println(l.getB().getBrand()); //M2
 
 
     }
