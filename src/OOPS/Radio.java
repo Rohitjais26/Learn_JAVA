9ee8ba83268becf7dcc54b600638d2cafc7bf078
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-08-25 23:38:42 +0530

    Learning OOPS concept like has-a-relationship  aggregation

---
+++ src/OOPS/Radio.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+public class Radio {
+    int rId;
+    String radiocapacity;
+
+    // Constructor
+    public Radio(int rId, String radiocapacity){
+        this.rId = rId;
+        this.radiocapacity = radiocapacity;
+    }
+
+}
