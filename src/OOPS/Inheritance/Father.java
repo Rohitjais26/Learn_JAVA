e9fbce4262b830f5c45b6b2a950402eb3e32f80d
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-08-27 00:29:03 +0530

    Learning OOPS concept Inheritance Parent And Child Relationship using child class name.

---
+++ src/OOPS/Inheritance/Father.java
@@ -0,0 +1,10 @@
+package OOPS.Inheritance;
+
+public class Father {
+    static {
+        System.out.println("Father Class is loading ");
+    }
+    public static void ridingBike(){
+        System.out.println("Father bike");
+    }
+}
---
+++ src/OOPS/Inheritance/Son.java
@@ -0,0 +1,10 @@
+package OOPS.Inheritance;
+
+public class Son extends Father{
+    static {
+        System.out.println("Son class is  loading ");
+    }
+    public static void drivingcar(){
+        System.out.println("Son is Driving Car");
+    }
+}
