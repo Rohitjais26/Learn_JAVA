cc6daf79b0e5ea0d0b3a3fe697c983e179acaa75
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-08-26 23:59:41 +0530

    Learning OOPS concept Inheritance Parent And Child Relationship

---
+++ src/OOPS/Inheritance/Driver1.java
@@ -0,0 +1,17 @@
+package OOPS.Inheritance;
+
+public class Driver1 {
+    public static void main(String[] args){
+        System.out.println("Main Started");
+        System.out.println(Parent.age);
+        System.out.println("MAin ended");
+
+    }
+}
+//Output:-
+//Main Started
+//Parent class is loading
+//30
+//MAin ended
+
+
