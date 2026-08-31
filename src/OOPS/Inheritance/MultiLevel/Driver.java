50ad75f6c033471526b66c48f3a1fd802fbf1b52
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-09-01 00:17:31 +0530

    Learning OOPS concept Multiple Level Inheritance

---
+++ src/OOPS/Inheritance/MultiLevel/Driver.java
@@ -0,0 +1,8 @@
+package OOPS.Inheritance.MultiLevel;
+
+public class Driver {
+    public static void main(String[] args){
+        Dept d = new Dept(80567,"Mumbai University",5478,"Pillai",302,"ECS");
+        d.printDDetails();
+    }
+}
